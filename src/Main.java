0ffb1cccca9624451af6eff63bcd38d265d1106a
aplicatii <vanessa.iluti@gmail.com>
2024-10-08 16:11:48 +0300

    Aufgabe1 1. + 2. + 3.

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,20 @@ import java.util.ArrayList;
 class Noten {
 
     public static void main(String[] args) {
-        int[] noten = {67, 34, 57, 90, 23, 89};
+        int[] noten = {29, 37, 38, 41, 84, 67};
         ArrayList<Integer> ergebniss_nichtAusreichend = nichtAusreichendeNoten(noten);
         double ergebniss_durchschnitt = durchschnitt(noten);
+        ArrayList<Integer> ergebniss_aberundeteNoten = abgerundeteNoten(noten);
         System.out.println(ergebniss_nichtAusreichend);
         System.out.println(ergebniss_durchschnitt);
+        System.out.println(ergebniss_aberundeteNoten);
     }
 
     public static ArrayList<Integer> nichtAusreichendeNoten(int[] noten) {
         ArrayList<Integer> nichtAusreichend = new ArrayList<>();
 
         for (int note : noten) {
-            if (note > 38) {
+            if (note >= 38) {
                 int multipleVon5 = ((note / 5) + 1) * 5;
                 if (multipleVon5 - note < 3) {
                     note = multipleVon5;
@@ -35,6 +37,21 @@ class Noten {
             summe += note;
         }
         return (double) summe / noten.length;
+    }
+    public static ArrayList<Integer> abgerundeteNoten(int[] noten){
+        ArrayList<Integer> abgerundeteNoten = new ArrayList<>();
+        for (int note : noten) {
+            if (note >= 38) {
+                int multipleVon5 = ((note / 5) + 1) * 5;
+                if (multipleVon5 - note < 3) {
+                    note = multipleVon5;
+                }
+            }
+
+            abgerundeteNoten.add(note);
+
+        }
+        return abgerundeteNoten;
     }
 }
 
