dcb896feef22e8a0da18be6bb7b6e0bd43a6e7f6
aplicatii <vanessa.iluti@gmail.com>
2024-10-13 22:51:33 +0300

    Aufgabe1 1. + 2. + 3. + 4. Aufgabe2 1. + 2. + 3. + 4. Aufgabe3 1. + 3. + 3. + 4.

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class Aufgabe1 {
 
@@ -124,5 +125,109 @@ class Aufgabe2{
             summe += zahl;
         }
         return summe - max;
+    }
+}
+
+class Augabe3{
+    public static void main(String[] args) {
+
+        int[] zahlen1 = {1, 3, 0, 0, 0, 0, 0, 0, 0};
+        int[] zahlen2 = {8, 7, 0, 0, 0, 0, 0, 0, 0};
+        int[] zahlen3 = {8, 3, 0, 0, 0, 0, 0, 0, 0};
+        int[] zahlen4 = {5, 4, 0, 0, 0, 0, 0, 0, 0};
+        int[] zahlen5 = {2, 3, 6, 0, 0, 0, 0, 0, 0};
+        int zahlen6 = 2;
+
+        int[] ergebniss_addierenGrosse = addierenGrosse(zahlen1, zahlen2);
+        int[] ergebniss_substrachierenGrosse = substrachierenGrosse(zahlen3, zahlen4);
+        int[] ergebniss_multiplizierenGrosse = multiplizierenGrosse(zahlen5, zahlen6);
+        int[] ergebniss_divisionGrosse = divisionGrosse(zahlen5, zahlen6);
+        System.out.println(Arrays.toString(ergebniss_addierenGrosse));
+        System.out.println(Arrays.toString(ergebniss_substrachierenGrosse));
+        System.out.println(Arrays.toString(ergebniss_multiplizierenGrosse));
+        System.out.println(Arrays.toString(ergebniss_divisionGrosse));
+    }
+
+    public static int[] addierenGrosse(int[] zahl1, int[] zahl2){
+        int len =  zahl1.length;
+        int[] result = new int[len + 1];
+        int carry = 0;
+
+        for(int i = len - 1; i >= 0; i--){
+            int summe = zahl1[i] + zahl2[i] + carry;
+            result[i + 1] = summe % 10;
+            carry = summe / 10;
+        }
+
+        result[0] = carry;
+
+        if (result[0] == 0) {
+            return Arrays.copyOfRange(result, 1, result.length);
+        }
+        return result;
+    }
+
+    public static int[] substrachierenGrosse(int[] zahl1, int[] zahl2){
+        int len =  zahl1.length;
+        int[] result = new int[len];
+        int ausleihen = 0;
+
+        for(int i = len - 1; i >= 0; i--){
+            int substrachieren = zahl1[i] - zahl2[i] - ausleihen;
+            if (substrachieren < 0) {
+                substrachieren += 10;
+                ausleihen = 1;
+            } else{
+                ausleihen = 0;
+            }
+            result[i]= substrachieren;
+        }
+
+        int fuehrendeNullenIndex = 0;
+        while (fuehrendeNullenIndex < result.length - 1 && result[fuehrendeNullenIndex] == 0) {
+            fuehrendeNullenIndex++;
+        }
+
+        return Arrays.copyOfRange(result, fuehrendeNullenIndex, result.length);
+    }
+
+    public static int[] multiplizierenGrosse(int[] zahl1, int zahl2){
+        int len =  zahl1.length;
+        int[] result = new int[len + 1];
+        int carry = 0;
+
+        for(int i = len - 1; i >= 0; i--){
+            int produkt = zahl1[i] * zahl2 + carry;
+            result[i + 1] = produkt % 10;
+            carry = produkt / 10;
+        }
+
+        result[0] = carry;
+
+        if (result[0] == 0) {
+            return Arrays.copyOfRange(result, 1, result.length);
+        }
+
+        return result;
+    }
+
+    public static int[] divisionGrosse(int[] zahl1, int zahl2){
+        int len =  zahl1.length;
+        int[] result = new int[len];
+        int carry = 0;
+
+        for(int i = 0; i < len; i++){
+            int division = carry * 10 + zahl1[i];
+            result[i] =  division / 2;
+            carry = division % zahl2;
+        }
+
+
+        int fuehrendeNullenIndex = 0;
+        while (fuehrendeNullenIndex < result.length - 1 && result[fuehrendeNullenIndex] == 0) {
+            fuehrendeNullenIndex++;
+        }
+
+        return Arrays.copyOfRange(result, fuehrendeNullenIndex, result.length);
     }
 }
